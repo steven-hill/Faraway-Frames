441e320ea808962522f80f2d5a594c5b19a4884c
steven-hill <stevenhill15@icloud.com>
2026-01-05 21:16:45 +0000

    Set up unit test target.

---
+++ Faraway Frames/Faraway FramesTests/Faraway_FramesTests.swift
@@ -0,0 +1,16 @@
+//
+//  Faraway_FramesTests.swift
+//  Faraway FramesTests
+//
+//  Created by Steven Hill on 05/01/2026.
+//
+
+import Testing
+
+struct Faraway_FramesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
