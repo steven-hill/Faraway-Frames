cf5ae2696f28a209db7a5b4f15457baf3747cd20
Steven Hill <98730693+steven-hill@users.noreply.github.com>
2026-01-05 19:58:23 +0000

    Initial commit
    - Set Swift language version to Swift 6.
    - Left Swift Concurrency settings as default.

---
+++ Faraway Frames/Faraway Frames/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Faraway Frames
+//
+//  Created by Steven Hill on 05/01/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
