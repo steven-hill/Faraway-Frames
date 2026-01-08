b1c2936be95f5d1170b66a8395a584c46dcb8ef4
steven-hill <stevenhill15@icloud.com>
2026-01-08 16:45:11 +0000

    Update `SceneDelegate` code

--- Faraway Frames/Faraway Frames/App/SceneDelegate.swift
+++ Faraway Frames/Faraway Frames/App/SceneDelegate.swift
@@ -13,9 +13,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = TabBarController()
-        window?.makeKeyAndVisible()
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = TabBarController()
+        window.makeKeyAndVisible()
+        self.window = window
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
