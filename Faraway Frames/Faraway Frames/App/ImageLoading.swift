fc6051ea338f96b7d35e863abae410c0af739bbb
steven-hill <stevenhill15@icloud.com>
2026-02-01 14:48:01 +0000

    Dependency container

---
+++ Faraway Frames/Faraway Frames/App/ImageLoading.swift
@@ -0,0 +1,12 @@
+//
+//  ImageLoading.swift
+//  Faraway Frames
+//
+//  Created by Steven Hill on 01/02/2026.
+//
+
+import Foundation
+
+protocol ImageLoading {
+    func makeImageLoader() -> ImageLoader
+}
