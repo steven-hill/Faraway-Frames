3a2a3214107cd16755abd691f10e77ed7a7f98fb
steven-hill <stevenhill15@icloud.com>
2026-03-02 14:26:28 +0000

    Refactor Explore Split View Coordinator unit tests

---
+++ Faraway Frames/Faraway FramesUnitTests/TestUtilities/IpadHelper.swift
@@ -0,0 +1,12 @@
+//
+//  IpadHelper.swift
+//  Faraway FramesTests
+//
+//  Created by Steven Hill on 02/03/2026.
+//
+
+import UIKit
+
+struct IpadHelper {
+    static let isPad = DispatchQueue.main.sync { UIDevice.current.userInterfaceIdiom == .pad }
+}
