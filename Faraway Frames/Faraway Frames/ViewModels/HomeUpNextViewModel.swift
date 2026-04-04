a5a4f848731ecb27f5a83e1c80a33698c14308dc
steven-hill <stevenhill15@icloud.com>
2026-04-04 20:42:45 +0100

    Added state for when UpNext has no films

--- Faraway Frames/Faraway Frames/ViewModels/HomeUpNextViewModel.swift
+++ Faraway Frames/Faraway Frames/ViewModels/HomeUpNextViewModel.swift
@@ -8,7 +8,14 @@
 import Foundation
 
 final class HomeUpNextViewModel {
+    
+    // MARK: - State Definition
+    enum HomeUpNextState {
+        case noFilms
+    }
+    
     // MARK: - Properties
+    private(set) var currentState: HomeUpNextState = .noFilms
     private(set) var upNextFilms: [Film] = []
     weak var delegate: HomeUpNextViewModelDelegate?
 }
