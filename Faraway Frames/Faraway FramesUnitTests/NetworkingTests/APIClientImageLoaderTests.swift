5738979fff154868338d883e263287905faf6111
steven-hill <stevenhill15@icloud.com>
2026-04-04 14:56:17 +0100

    Add unit test to check URLCache

--- Faraway Frames/Faraway FramesUnitTests/NetworkingTests/APIClientImageLoaderTests.swift
+++ Faraway Frames/Faraway FramesUnitTests/NetworkingTests/APIClientImageLoaderTests.swift
@@ -48,6 +48,35 @@ struct APIClientImageLoaderTests {
         #expect(cacheManager.cachedData.keys.first == "https://example.com/image.png", "Cache should have stored the correct key.")
     }
     
+    @Test func apiClientImageLoader_whenImageIsInURLCacheButNotNSCache_shouldRetrieveFromURLCacheAndAddToNSCache() async {
+        let cacheManager = MockCacheManager()
+        let testURL = makeTestURL()
+        let testImage = makeTestImage()
+        let imageData = testImage.pngData()!
+        
+        let mockResponse = HTTPURLResponse(
+            url: testURL,
+            statusCode: 200,
+            httpVersion: nil,
+            headerFields: nil)!
+                
+        let testCache = URLCache(memoryCapacity: 10 * 1024, diskCapacity: 10 * 1024, diskPath: "test_URLCache")
+        let cachedResponse = CachedURLResponse(response: mockResponse, data: imageData)
+        testCache.removeAllCachedResponses()
+        testCache.storeCachedResponse(cachedResponse, for: URLRequest(url: testURL))
+        let configuration = URLSessionConfiguration.default
+        configuration.urlCache = testCache
+        let session = StubNetworkSession(configuration: configuration)
+        
+        let sut = APIClientImageLoader(session: session, cacheManager: cacheManager)
+        
+        let retrievedImage = await sut.loadImage(from: testURL)
+        
+        #expect(cacheManager.getDataCalled == true, "Should have checked NSCache first.")
+        #expect(retrievedImage != nil, "Should have retrieved image from URLCache.")
+        #expect(cacheManager.setDataCalled == true, "Should have saved the URLCache result back into NSCache for next time.")
+    }
+    
     // MARK: - Helper methods
     private func makeURLString() -> String {
         "https://example.com/image.png"
