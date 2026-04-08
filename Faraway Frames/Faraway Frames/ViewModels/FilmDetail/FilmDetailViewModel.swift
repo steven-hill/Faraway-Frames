0705f8c7749afe69dca89d4b35e447ad95166abd
steven-hill <stevenhill15@icloud.com>
2026-04-08 10:52:43 +0100

    Remove conformance to `FilmDetailViewModelProtocol` from `FilmDetailViewModel`

--- Faraway Frames/Faraway Frames/ViewModels/FilmDetail/FilmDetailViewModel.swift
+++ Faraway Frames/Faraway Frames/ViewModels/FilmDetail/FilmDetailViewModel.swift
@@ -8,7 +8,7 @@
 import Foundation
 import UIKit
 
-final class FilmDetailViewModel: FilmDetailViewModelProtocol {
+final class FilmDetailViewModel {
     
     // MARK: - State Definition
     enum FilmDetailState: Equatable {
