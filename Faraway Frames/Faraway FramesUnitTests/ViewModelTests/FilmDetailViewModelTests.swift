74936a18f37ec30b3edc4d3c2e896599c4cd021e
steven-hill <stevenhill15@icloud.com>
2026-04-08 10:25:18 +0100

    Remove the unit test for `cancelImageLoad()` from `FilmDetailViewModel` unit tests

--- Faraway Frames/Faraway FramesUnitTests/ViewModelTests/FilmDetailViewModelTests.swift
+++ Faraway Frames/Faraway FramesUnitTests/ViewModelTests/FilmDetailViewModelTests.swift
@@ -97,17 +97,6 @@ struct FilmDetailViewModelTests {
         }
     }
     
-    @Test(.tags(.networkRequest)) func filmDetailViewModel_cancelImageLoad_marksTaskForCancellationAndSetsItToNil() {
-        let sut = makeSUT()
-        sut.setFilm(Film.sample[0])
-        let capturedTask = sut.imageLoadTask
-        
-        sut.cancelImageLoad()
-        
-        #expect(capturedTask?.isCancelled == true, "Should be marked for cancellation.")
-        #expect(sut.imageLoadTask == nil, "Should be nil.")
-    }
-    
     //MARK: - Helper method
     private func makeSUT() -> FilmDetailViewModel {
         let mockImageLoader = MockImageLoader()
