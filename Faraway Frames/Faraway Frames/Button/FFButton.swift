684505522d777ea8bd02e7e98c011e1e032a8c90
steven-hill <stevenhill15@icloud.com>
2026-04-10 18:53:55 +0100

    Implement the custom configuration of `FFButton`.

--- Faraway Frames/Faraway Frames/Button/FFButton.swift
+++ Faraway Frames/Faraway Frames/Button/FFButton.swift
@@ -7,6 +7,41 @@
 
 import UIKit
 
-class FFButton: UIButton {
-
+final class FFButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    convenience init(title: String, accessibilityIdentifier: String) {
+        self.init(frame: .zero)
+        set(title: title, accessibilityIdentifier: accessibilityIdentifier)
+    }
+    
+    private func configure() {
+        configuration = .filled()
+        configuration?.baseBackgroundColor = .secondarySystemBackground
+        configuration?.baseForegroundColor = .label
+        configuration?.background.cornerRadius = 10
+        configuration?.titleLineBreakMode = .byWordWrapping
+        configuration?.titleAlignment = .center
+        configuration?.titleTextAttributesTransformer = UIConfigurationTextAttributesTransformer { incoming in
+            var outgoing = incoming
+            outgoing.font = UIFont.preferredFont(forTextStyle: .body)
+            return outgoing
+        }
+        configuration?.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
+        translatesAutoresizingMaskIntoConstraints = false
+        heightAnchor.constraint(greaterThanOrEqualToConstant: 44).isActive = true
+    }
+    
+    func set(title: String, accessibilityIdentifier: String) {
+        configuration?.title = title
+        self.accessibilityIdentifier = accessibilityIdentifier
+    }
 }
