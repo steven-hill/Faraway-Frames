ff7d9b0a4e396ea6846f54bf94a533940e630671
steven-hill <stevenhill15@icloud.com>
2026-04-16 21:37:27 +0100

    Add unit test for `FilmsListAPIClient` to check `URLCache` for films data before making network request.

--- Faraway Frames/Faraway FramesUnitTests/NetworkingTests/FilmsListAPIClientTests.swift
+++ Faraway Frames/Faraway FramesUnitTests/NetworkingTests/FilmsListAPIClientTests.swift
@@ -13,6 +13,28 @@ import Foundation
 struct FilmsListAPIClientTests {
     
     @Test(.tags(.networkRequest, .decoding))
+    func filmsListAPIClient_fetchAllFilms_ifURLCacheDataExists_usesURLCacheData() async throws {
+        let testURL = URL(string: makeFilmsURLString())
+        let mockResponse = HTTPURLResponse(
+            url: testURL!,
+            statusCode: 200,
+            httpVersion: nil,
+            headerFields: nil)!
+        let testCache = URLCache(memoryCapacity: 10 * 1024, diskCapacity: 10 * 1024, diskPath: "test_FilmsList_URLCache")
+        let cachedResponse = CachedURLResponse(response: mockResponse, data: makeTestURLCacheFilmsData())
+        testCache.removeAllCachedResponses()
+        testCache.storeCachedResponse(cachedResponse, for: URLRequest(url: testURL!))
+        let configuration = URLSessionConfiguration.ephemeral
+        configuration.urlCache = testCache
+        let session = StubNetworkSession(configuration: configuration)
+        let sut = FilmsListAPIClient(session: session)
+
+        let films = try await sut.fetchAllFilms()
+        
+        #expect(films.count == 2, "Should be two films as in the cached mock data.")
+    }
+    
+    @Test(.tags(.networkRequest, .decoding))
     func filmsListAPIClient_fetchAllFilms_decodesDataOn200Response_withCorrectURL() async throws {
         let mockData = makeValidMockFilmsData()
         let mockResponse = HTTPURLResponse(
@@ -87,6 +109,68 @@ struct FilmsListAPIClientTests {
     private func makeSUT(data: Data, response: URLResponse) -> FilmsListAPIClient {
         let session = StubNetworkSession(data: data, response: response)
         return FilmsListAPIClient(session: session)
+    }
+    
+    private func makeTestURLCacheFilmsData() -> Data {
+        let json = """
+            [
+                {
+                  "id": "2baf70d1-42bb-4437-b551-e5fed5a87abe",
+                  "title": "Castle in the Sky",
+                  "original_title": "天空の城ラピュタ",
+                  "original_title_romanised": "Tenkū no shiro Rapyuta",
+                  "image": "https://image.tmdb.org/t/p/w600_and_h900_bestv2/npOnzAbLh6VOIu3naU5QaEcTepo.jpg",
+                  "movie_banner": "https://image.tmdb.org/t/p/w533_and_h300_bestv2/3cyjYtLWCBE1uvWINHFsFnE8LUK.jpg",
+                  "description": "The orphan Sheeta inherited a mysterious crystal that links her to the mythical sky-kingdom of Laputa...",
+                  "director": "Hayao Miyazaki",
+                  "producer": "Isao Takahata",
+                  "release_date": "1986",
+                  "running_time": "124",
+                  "rt_score": "95",
+                  "people": [
+                    "https://ghibliapi.vercel.app/people/598f7048-74ff-41e0-92ef-87dc1ad980a9"
+                  ],
+                  "species": [
+                    "https://ghibliapi.vercel.app/species/af3910a6-429f-4c74-9ad5-dfe1c4aa04f2"
+                  ],
+                  "locations": [
+                    "https://ghibliapi.vercel.app/locations/"
+                  ],
+                  "vehicles": [
+                    "https://ghibliapi.vercel.app/vehicles/4e09b023-f650-4747-9ab9-eacf14540cfb"
+                  ],
+                  "url": "https://ghibliapi.vercel.app/films/2baf70d1-42bb-4437-b551-e5fed5a87abe"
+                },
+              {
+                "id": "12cfb892-aac0-4c5b-94af-521852e46d6a",
+                "title": "Grave of the Fireflies",
+                "original_title": "火垂るの墓",
+                "original_title_romanised": "Hotaru no haka",
+                "image": "https://image.tmdb.org/t/p/w600_and_h900_bestv2/qG3RYlIVpTYclR9TYIsy8p7m7AT.jpg",
+                "movie_banner": "https://image.tmdb.org/t/p/original/vkZSd0Lp8iCVBGpFH9L7LzLusjS.jpg",
+                "description": "In the latter part of World War II, a boy and his sister, orphaned when their mother is killed in the firebombing of Tokyo, are left to survive on their own in what remains of civilian life in Japan. The plot follows this boy and his sister as they do their best to survive in the Japanese countryside, battling hunger, prejudice, and pride in their own quiet, personal battle.",
+                "director": "Isao Takahata",
+                "producer": "Toru Hara",
+                "release_date": "1988",
+                "running_time": "89",
+                "rt_score": "97",
+                "people": [
+                  "https://ghibliapi.vercel.app/people/"
+                ],
+                "species": [
+                  "https://ghibliapi.vercel.app/species/af3910a6-429f-4c74-9ad5-dfe1c4aa04f2"
+                ],
+                "locations": [
+                  "https://ghibliapi.vercel.app/locations/"
+                ],
+                "vehicles": [
+                  "https://ghibliapi.vercel.app/vehicles/"
+                ],
+                "url": "https://ghibliapi.vercel.app/films/12cfb892-aac0-4c5b-94af-521852e46d6a"
+              }
+            ]
+            """
+        return Data(json.utf8)
     }
     
     private func makeValidMockFilmsData() -> Data {
