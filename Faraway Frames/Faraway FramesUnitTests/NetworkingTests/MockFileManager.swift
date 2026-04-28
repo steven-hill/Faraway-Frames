d8ad850413feb081b16f7112e85871d5c60196ea
steven-hill <stevenhill15@icloud.com>
2026-04-28 19:20:49 +0100

    Create `MockFileManager` for unit testing

---
+++ Faraway Frames/Faraway FramesUnitTests/NetworkingTests/MockFileManager.swift
@@ -0,0 +1,19 @@
+//
+//  MockFileManager.swift
+//  Faraway Frames
+//
+//  Created by Steven Hill on 28/04/2026.
+//
+
+import Foundation
+
+final class MockFileManager: FileManaging {
+    var mockFiles: [String: Data] = [:]
+    var writeWasCalled = false
+    
+    func createFile(atPath path: String, contents data: Data?, attributes attr: [FileAttributeKey : Any]?) -> Bool {
+        writeWasCalled = true
+        mockFiles[path] = data
+        return true
+    }
+}
