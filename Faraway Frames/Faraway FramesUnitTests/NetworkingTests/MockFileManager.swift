963080de99c134a0674842e8f85b1fa740c998a8
steven-hill <stevenhill15@icloud.com>
2026-05-02 18:23:08 +0100

    Rewrote `MockFileManager` to conform to modified protocol

--- Faraway Frames/Faraway FramesUnitTests/NetworkingTests/MockFileManager.swift
+++ Faraway Frames/Faraway FramesUnitTests/NetworkingTests/MockFileManager.swift
@@ -9,26 +9,34 @@ import Foundation
 @testable import Faraway_Frames
 
 final class MockFileManager: FileManaging {
-    var mockFiles: [String: Data] = [:]
-    let filePath = "/dev/null"
-    var writeWasCalled = false
+    var mockStorage: [URL: Data] = [:]
+    var didCreateDirectory = false
     var readWasCalled = false
+    var writeWasCalled = false
     
-    func urls(for directory: FileManager.SearchPathDirectory, in domainMask: FileManager.SearchPathDomainMask) -> [URL] {
-        return [URL(filePath: filePath, directoryHint: .isDirectory)]
+    func fileExists(atPath path: String) -> Bool {
+        let url = URL(filePath: path)
+        return mockStorage[url] != nil
     }
     
-    func createFile(atPath path: String, contents data: Data?, attributes attr: [FileAttributeKey : Any]?) -> Bool {
-        writeWasCalled = true
-        mockFiles[path] = data
-        return true
-    }
-    
-    func contents(atPath path: String) -> Data? {
+    func read(from url: URL) throws -> Data {
         readWasCalled = true
-        if let data = mockFiles[path] {
+        if let data = mockStorage[url] {
             return data
         }
-        return nil
+        throw NSError(domain: NSCocoaErrorDomain, code: NSFileReadNoSuchFileError)
+    }
+    
+    func write(data: Data, to url: URL, options: Data.WritingOptions) throws {
+        writeWasCalled = true
+        mockStorage[url] = data
+    }
+    
+    func createDirectory(at url: URL, withIntermediateDirectories: Bool, attributes: [FileAttributeKey : Any]?) throws {
+        didCreateDirectory = true
+    }
+    
+    func urls(for directory: FileManager.SearchPathDirectory, in domainMask: FileManager.SearchPathDomainMask) -> [URL] {
+        return [URL(filePath: "/mock/directory")]
     }
 }
