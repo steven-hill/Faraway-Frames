60d5a149d8a377af63ef824c3e63e2aa1f817bc9
steven-hill <stevenhill15@icloud.com>
2026-05-04 17:13:11 +0100

    Add `GhibliAPI` enum with string for the `URL`

---
+++ Faraway Frames/Faraway Frames/Constants/GhibliAPI.swift
@@ -0,0 +1,12 @@
+//
+//  GhibliAPI.swift
+//  Faraway Frames
+//
+//  Created by Steven Hill on 04/05/2026.
+//
+
+import Foundation
+
+enum GhibliAPI {
+    static let urlString = "https://ghibliapi.vercel.app/films"
+}
