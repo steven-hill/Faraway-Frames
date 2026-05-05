f26bb65f0b4b9dfdc6efdd19242b477d323fbf6a
steven-hill <stevenhill15@icloud.com>
2026-05-05 17:37:52 +0100

    Change cache policy in `APIClientImageLoader` to `.useProtocolCachePolicy`

--- Faraway Frames/Faraway Frames/Networking/APIClientImageLoader.swift
+++ Faraway Frames/Faraway Frames/Networking/APIClientImageLoader.swift
@@ -14,7 +14,7 @@ final class APIClientImageLoader: ImageLoader {
     init(session: NetworkSession? = nil, cacheManager: CacheManagerProtocol) {
         let config = URLSessionConfiguration.default
         config.urlCache = URLCache(memoryCapacity: 50 * 1024 * 1024, diskCapacity: 200 * 1024 * 1024, diskPath: "com.stevenhill.farawayframes.cache")
-        config.requestCachePolicy = .returnCacheDataElseLoad
+        config.requestCachePolicy = .useProtocolCachePolicy
         self.session = session ?? URLSession(configuration: config)
         self.cacheManager = cacheManager
     }
