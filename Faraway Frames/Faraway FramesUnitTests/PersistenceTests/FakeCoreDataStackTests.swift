b6bac892cf0eb070491322655a4def5287b8b0fb
Steven Hill <98730693+steven-hill@users.noreply.github.com>
2026-05-26 19:38:24 +0100

    Create folder, unit test file and uni test for fake Core Data stack

---
+++ Faraway Frames/Faraway FramesUnitTests/PersistenceTests/FakeCoreDataStackTests.swift
@@ -0,0 +1,14 @@
+//
+//  FakeCoreDataStackTests.swift
+//  Faraway FramesTests
+//
+//  Created by Steven Hill on 26/05/2026.
+//
+
+import Testing
+
+struct FakeCoreDataStackTests {
+
+    @Test func fakeCoreDataStack_configuresInMemoryContainerAndStoresItInNullDirectory() {
+    }
+}
