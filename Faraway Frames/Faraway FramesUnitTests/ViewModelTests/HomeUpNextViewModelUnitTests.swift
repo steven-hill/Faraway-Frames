278dd334db1ab10f126442097fdc08b8baadc40e
Steven Hill <98730693+steven-hill@users.noreply.github.com>
2026-05-27 19:44:45 +0100

    Add `HomeUpNextViewModelDelegateSpy` and update test's expectations

--- Faraway Frames/Faraway FramesUnitTests/ViewModelTests/HomeUpNextViewModelUnitTests.swift
+++ Faraway Frames/Faraway FramesUnitTests/ViewModelTests/HomeUpNextViewModelUnitTests.swift
@@ -51,8 +51,21 @@ struct HomeUpNextViewModelUnitTests {
         sut.startFetching()
         
         #expect(delegateSpy.callCount == 1, "Should make the call once.")
-        #expect(delegateSpy.updatedFilms.count == 1, "Should be one.")
-        #expect(delegateSpy.updatedFilms.first.id == Film.sample[0].id, "Shoulld be equal.")
-        #expect(delegateSpy.updatedFilms.first.film.title == Film.sample[0].title, "Should be equal.")
+        let films = try #require(delegateSpy.updatedFilms, "Delegate should have received a films array.")
+        #expect(films.count == 1, "Should be one.")
+        let firstFilm = try #require(films.first, "The film array should contain a film.")
+        #expect(firstFilm.id == Film.sample[0].id, "Should be equal.")
+        #expect(firstFilm.film.title == Film.sample[0].title, "Should be equal.")
+    }
+    
+    //MARK: - Home UpNext ViewModel Delegate Spy
+    final class HomeUpNextViewModelDelegateSpy: HomeUpNextViewModelDelegate {
+        var updatedFilms: [FilmWithStatus]?
+        var callCount: Int = 0
+        
+        func upNextFilmsDidUpdate(films: [FilmWithStatus]) {
+            self.updatedFilms = films
+            self.callCount += 1
+        }
     }
 }
