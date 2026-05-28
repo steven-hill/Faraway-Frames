2dba39d2468860cbf2763f44d1e567981961613f
Steven Hill <98730693+steven-hill@users.noreply.github.com>
2026-05-28 17:17:38 +0100

    Created new unit test file with first test

---
+++ Faraway Frames/Faraway FramesUnitTests/PersistenceTests/CoreDataStackTests.swift
@@ -0,0 +1,14 @@
+//
+//  CoreDataStackTests.swift
+//  Faraway FramesTests
+//
+//  Created by Steven Hill on 28/05/2026.
+//
+
+import Testing
+
+struct CoreDataStackTests {
+
+    @Test func coreDataStack_canInit() {
+    }
+}
