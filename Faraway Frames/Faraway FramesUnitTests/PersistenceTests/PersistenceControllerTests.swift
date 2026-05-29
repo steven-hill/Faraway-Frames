1e26994d180852bcd75d1e0c10986dda3ee733b7
Steven Hill <98730693+steven-hill@users.noreply.github.com>
2026-05-29 21:30:37 +0100

    Add unit tests to check saving fails if one attrivute or multiple attributes are nil

--- Faraway Frames/Faraway FramesUnitTests/PersistenceTests/PersistenceControllerTests.swift
+++ Faraway Frames/Faraway FramesUnitTests/PersistenceTests/PersistenceControllerTests.swift
@@ -9,6 +9,7 @@ import Testing
 import CoreData
 @testable import Faraway_Frames
 
+@MainActor
 struct PersistenceControllerTests {
     
     @Test("Verify error thrown when loading persistent stores fails")
@@ -24,7 +25,7 @@ struct PersistenceControllerTests {
         #expect(thrownError == .loadingStoresFailed(error: mockError))
     }
     
-    @MainActor @Test("Verify Core Data stack can save and fetch films")
+    @Test("Verify Core Data stack can save and fetch films")
     func persistenceController_canFetchSavedFilms() throws {
         let sut = try PersistenceController(inMemory: true)
         let context = sut.viewContext
@@ -64,6 +65,70 @@ struct PersistenceControllerTests {
             
             let isUpNextValue = retrievedFilm.value(forKey: "isUpNext") as? Bool
             #expect(isUpNextValue == true, "The tracking state flag should remain intact.")
+        }
+    }
+    
+    @Test("Verify saving failed due to single nil attribute throws error")
+    func persistenceController_whenSavingFailedDueToSingleNilAttribute_throwsError() throws {
+        let sut = try PersistenceController.init(inMemory: true)
+        let context = sut.viewContext
+        let entity = try #require(
+            NSEntityDescription.entity(forEntityName: "FilmMO", in: context),
+            "The Core Data model schema must contain an entity definition named 'FilmMO'."
+        )
+        
+        let sampleFilm = Film.sample[0]
+        let upNextFilm = FilmMO(entity: entity, insertInto: context)
+        upNextFilm.title = sampleFilm.title
+        upNextFilm.originalTitle = sampleFilm.originalTitle
+        upNextFilm.originalTitleRomanised = sampleFilm.originalTitleRomanised
+        upNextFilm.image = sampleFilm.image
+        upNextFilm.movieBanner = sampleFilm.movieBanner
+        upNextFilm.filmDescription = sampleFilm.description
+        upNextFilm.director = sampleFilm.director
+        upNextFilm.producer = sampleFilm.producer
+        upNextFilm.releaseDate = sampleFilm.releaseDate
+        upNextFilm.runningTime = sampleFilm.runningTime
+        upNextFilm.rottenTomatoesScore = sampleFilm.rottenTomatoesScore
+        upNextFilm.url = sampleFilm.url
+        upNextFilm.isUpNext = true
+        
+        let thrownError = #expect(throws: PersistenceError.self) {
+            try sut.saveContext()
+        }
+        switch thrownError {
+        case .savingFailed(let error):
+            let nsError = error as NSError
+            #expect(nsError.domain == NSCocoaErrorDomain, "Should be Cocoa Error.")
+            #expect(nsError.code == NSValidationMissingMandatoryPropertyError, "Should be NSValidationMissingMandatoryPropertyError because one attribute (.id) is nil.")
+        default:
+            Issue.record("Expected a .savingFailed error, but got \(thrownError)")
+        }
+    }
+    
+    @Test("Verify saving failed due to multiple nil attributes throws error")
+    func persistenceController_whenSavingFailedDueToMultipleNilAttributes_throwsError() throws {
+        let sut = try PersistenceController.init(inMemory: true)
+        let context = sut.viewContext
+        let entity = try #require(
+            NSEntityDescription.entity(forEntityName: "FilmMO", in: context),
+            "The Core Data model schema must contain an entity definition named 'FilmMO'."
+        )
+        
+        let sampleFilm = Film.sample[0]
+        let upNextFilm = FilmMO(entity: entity, insertInto: context)
+        upNextFilm.id = sampleFilm.id
+        
+        let thrownError = #expect(throws: PersistenceError.self) {
+            try sut.saveContext()
+        }
+        switch thrownError {
+        case .savingFailed(let error):
+            let nsError = error as NSError
+            #expect(nsError.domain == NSCocoaErrorDomain, "Should be Cocoa Error.")
+            #expect(nsError.code == NSValidationMultipleErrorsError, "Should be NSValidationMultipleErrorsError because multiple attributes are nil.")
+        default:
+            Issue.record("Expected a .savingFailed error, but got \(thrownError)")
         }
     }
 }
