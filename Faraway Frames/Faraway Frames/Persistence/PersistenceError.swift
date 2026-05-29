9d23d99b7fa2c48ec53684e224899c2e8da5af51
Steven Hill <98730693+steven-hill@users.noreply.github.com>
2026-05-29 21:34:10 +0100

    Add case for saving failed to `PersistenceError`

--- Faraway Frames/Faraway Frames/Persistence/PersistenceError.swift
+++ Faraway Frames/Faraway Frames/Persistence/PersistenceError.swift
@@ -9,13 +9,18 @@ import Foundation
 
 enum PersistenceError: Error {
     case loadingStoresFailed(error: Error)
+    case savingFailed(error: Error)
 }
 
 extension PersistenceError: Equatable {
     static func == (lhs: PersistenceError, rhs: PersistenceError) -> Bool {
         switch (lhs, rhs) {
-        case(.loadingStoresFailed(let lhsType), .loadingStoresFailed(let rhsType)):
+        case (.loadingStoresFailed(let lhsType), .loadingStoresFailed(let rhsType)):
             return lhsType.localizedDescription == rhsType.localizedDescription
+        case (.savingFailed(let lhsType), .savingFailed(let rhsType)):
+            return lhsType.localizedDescription == rhsType.localizedDescription
+        default:
+            return false
         }
     }
 }
