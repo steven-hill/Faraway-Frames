6daa25d3c73309944488c7c4d04db6e2656cb0f4
Steven Hill <98730693+steven-hill@users.noreply.github.com>
2026-06-01 15:34:16 +0100

    Replace private helper methdo on the test suite with the method from the test utilities

--- Faraway Frames/Faraway FramesUnitTests/ViewModelTests/HomeUpNextViewModelUnitTests.swift
+++ Faraway Frames/Faraway FramesUnitTests/ViewModelTests/HomeUpNextViewModelUnitTests.swift
@@ -19,7 +19,7 @@ struct HomeUpNextViewModelUnitTests {
         #expect(sut.currentState == .noFilms, "Should be `.noFilms` on init.")
     }
     
-    @Test("Verify `HomeUpNextViewModel` fetches only Up Next records")
+    @Test("Verify `HomeUpNextViewModel` only fetches Up Next records")
     func homeUpNextViewModel_fetchesCorrectly() throws {
         let persistenceController = try PersistenceController(inMemory: true)
         let sut = HomeUpNextViewModel(persistentContainer: persistenceController.container)
@@ -30,8 +30,9 @@ struct HomeUpNextViewModelUnitTests {
             NSEntityDescription.entity(forEntityName: "FilmMO", in: context),
             "The Core Data model schema must contain an entity definition named 'FilmMO'."
         )
-        _ = makeFilmToBeSaved(with: Film.sample[0], entity: entity, context: context, isUpNext: true, isWatched: false)
-        _ = makeFilmToBeSaved(with: Film.sample[1], entity: entity, context: context, isUpNext: false, isWatched: true)
+        
+        _ = PersistenceHelper.makeFilmMO(with: Film.sample[0], entity: entity, context: context, isUpNext: true, isWatched: false)
+        _ = PersistenceHelper.makeFilmMO(with: Film.sample[1], entity: entity, context: context, isUpNext: false, isWatched: true)
         try context.save()
         
         sut.startFetching()
@@ -42,27 +43,6 @@ struct HomeUpNextViewModelUnitTests {
         let firstFilm = try #require(films.first, "The film array should contain a film.")
         #expect(firstFilm.id == Film.sample[0].id, "Should be equal.")
         #expect(firstFilm.film.title == Film.sample[0].title, "Should be equal.")
-    }
-    
-    // MARK: - Helper method
-    private func makeFilmToBeSaved(with film: Film, entity: NSEntityDescription, context: NSManagedObjectContext, isUpNext: Bool, isWatched: Bool) -> FilmMO {
-        let filmToBeSaved = FilmMO(entity: entity, insertInto: context)
-        filmToBeSaved.id = film.id
-        filmToBeSaved.title = film.title
-        filmToBeSaved.originalTitle = film.originalTitle
-        filmToBeSaved.originalTitleRomanised = film.originalTitleRomanised
-        filmToBeSaved.image = film.image
-        filmToBeSaved.movieBanner = film.movieBanner
-        filmToBeSaved.filmDescription = film.description
-        filmToBeSaved.director = film.director
-        filmToBeSaved.producer = film.producer
-        filmToBeSaved.releaseDate = film.releaseDate
-        filmToBeSaved.runningTime = film.runningTime
-        filmToBeSaved.rottenTomatoesScore = film.rottenTomatoesScore
-        filmToBeSaved.url = film.url
-        filmToBeSaved.isUpNext = isUpNext
-        filmToBeSaved.isWatched = isWatched
-        return filmToBeSaved
     }
     
     //MARK: - Home UpNext ViewModel Delegate Spy
