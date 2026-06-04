3cb651f004e21fcdeaf106f2f7ed4fd32c354809
Steven Hill <98730693+steven-hill@users.noreply.github.com>
2026-06-04 12:11:32 +0100

    add first test for init

--- Faraway Frames/Faraway FramesUnitTests/ViewControllerTests/DatabaseErrorVCTests.swift
+++ Faraway Frames/Faraway FramesUnitTests/ViewControllerTests/DatabaseErrorVCTests.swift
@@ -6,9 +6,17 @@
 //
 
 import Testing
+import UIKit
+@testable import Faraway_Frames
 
+@MainActor
 struct DatabaseErrorVCTests {
 
-    @Test func databaseErrorVC() {
+    @Test func databaseErrorVC_canInitAndLoadView() {
+        let sut = DatabaseErrorVC()
+        
+        sut.loadViewIfNeeded()
+        
+        #expect(sut.view != nil, "VC should load the view.")
     }
 }
