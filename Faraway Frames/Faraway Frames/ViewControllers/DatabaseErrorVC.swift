9bfab160af5ceb4d0a3c84be9b28b65328d11410
Steven Hill <98730693+steven-hill@users.noreply.github.com>
2026-06-04 12:01:33 +0100

    created the unit test file and the production file

---
+++ Faraway Frames/Faraway Frames/ViewControllers/DatabaseErrorVC.swift
@@ -0,0 +1,15 @@
+//
+//  DatabaseErrorVC.swift
+//  Faraway Frames
+//
+//  Created by Steven Hill on 04/06/2026.
+//
+
+import UIKit
+
+final class DatabaseErrorVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ Faraway Frames/Faraway FramesUnitTests/ViewControllerTests/DatabaseErrorVCTests.swift
@@ -0,0 +1,14 @@
+//
+//  DatabaseErrorVCTests.swift
+//  Faraway FramesTests
+//
+//  Created by Steven Hill on 04/06/2026.
+//
+
+import Testing
+
+struct DatabaseErrorVCTests {
+
+    @Test func databaseErrorVC() {
+    }
+}
