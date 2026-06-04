e210fe802df2a069ae6d0aa99b1d80c5b88f4e3f
Steven Hill <98730693+steven-hill@users.noreply.github.com>
2026-06-04 12:56:51 +0100

    add UI implementation to VC

--- Faraway Frames/Faraway Frames/ViewControllers/DatabaseErrorVC.swift
+++ Faraway Frames/Faraway Frames/ViewControllers/DatabaseErrorVC.swift
@@ -12,6 +12,7 @@ final class DatabaseErrorVC: UIViewController {
     //MARK: - Property
     private let errorMessage: String
     
+    //MARK: - Initialisation
     init(errorMessage: String) {
         self.errorMessage = errorMessage
         super.init(nibName: nil, bundle: nil)
@@ -21,7 +22,25 @@ final class DatabaseErrorVC: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
+    //MARK: - Lifecycle method
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        setupErrorUI()
     }
+    
+    private func setupErrorUI() {
+        var config = UIContentUnavailableConfiguration.empty()
+        config.text = "Database Error"
+        config.secondaryText = errorMessage + " Check device storage and try relaunching the app."
+        config.image = SFSymbols.exclamationMarkTriangle
+        config.imageProperties.tintColor = .systemRed
+        self.contentUnavailableConfiguration = config
+    }
+}
+
+//MARK: - Preview
+#Preview("Database Error VC") {
+    let vc = DatabaseErrorVC(errorMessage: "Failed to load database. Check device storage and try relaunching the app.")
+    vc
 }
