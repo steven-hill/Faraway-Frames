ffb5aca48257ac71dc137a644590c78a10563efc
Steven Hill <98730693+steven-hill@users.noreply.github.com>
2026-06-06 14:46:18 +0100

    Add new view model test file

---
+++ Faraway Frames/Faraway FramesUnitTests/ViewModelTests/HomeViewModelTests.swift
@@ -0,0 +1,15 @@
+//
+//  HomeViewModelTests.swift
+//  Faraway FramesTests
+//
+//  Created by Steven Hill on 06/06/2026.
+//
+
+import Testing
+
+struct HomeViewModelTests {
+
+    @Test func doNothing() {
+    }
+
+}
