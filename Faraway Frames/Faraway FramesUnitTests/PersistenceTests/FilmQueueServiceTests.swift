691274de1e15c574bd12d515080480f00daffc43
Steven Hill <98730693+steven-hill@users.noreply.github.com>
2026-06-17 12:18:04 +0100

    add test file

---
+++ Faraway Frames/Faraway FramesUnitTests/PersistenceTests/FilmQueueServiceTests.swift
@@ -0,0 +1,14 @@
+//
+//  FilmQueueServiceTests.swift
+//  Faraway FramesTests
+//
+//  Created by Steven Hill on 17/06/2026.
+//
+
+import Testing
+
+struct FilmQueueServiceTests {
+
+    @Test func doNothing() {
+    }
+}
