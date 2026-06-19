3acfa1b7dc084ab5371eb3215cf09206cf997362
Steven Hill <98730693+steven-hill@users.noreply.github.com>
2026-06-19 22:00:16 +0100

    add test file

---
+++ Faraway Frames/Faraway FramesUnitTests/PersistenceTests/FilmSyncServiceTests.swift
@@ -0,0 +1,14 @@
+//
+//  FilmSyncServiceTests.swift
+//  Faraway FramesTests
+//
+//  Created by Steven Hill on 19/06/2026.
+//
+
+import Testing
+
+struct FilmSyncServiceTests {
+
+    @Test func doNothing() {
+    }
+}
