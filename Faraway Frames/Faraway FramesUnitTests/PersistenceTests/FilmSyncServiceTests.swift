3022804e7a776651bcf7b0e4590c1ca3ed23b600
Steven Hill <98730693+steven-hill@users.noreply.github.com>
2026-06-20 13:08:38 +0100

    extract sut creation into helper method

--- Faraway Frames/Faraway FramesUnitTests/PersistenceTests/FilmSyncServiceTests.swift
+++ Faraway Frames/Faraway FramesUnitTests/PersistenceTests/FilmSyncServiceTests.swift
@@ -13,9 +13,8 @@ import CoreData
 struct FilmSyncServiceTests {
 
     @Test("Should exit immediately if input films array is empty")
-    func filmSyncService_syncFilmsWithLocalStorage_ifFilmsArrayIsEmpty_exitsImmediately() async {
-        let testPersistenceController = try! PersistenceController(inMemory: true)
-        let sut = FilmSyncService(context: testPersistenceController.viewContext)
+    func filmSyncService_syncFilmsWithLocalStorage_ifFilmsArrayIsEmpty_exitsImmediately() async throws {
+        let (sut, _, _) = try makeSUTViewContextAndEntity()
         let films: [Film] = []
         
         let result = await sut.syncFilmsWithLocalStorage(films)
@@ -24,9 +23,8 @@ struct FilmSyncServiceTests {
     }
 
     @Test("If database is empty, should return the input films with their default flags unchanged.")
-    func filmSyncService_syncFilmsWithLocalStorage_ifDatabaseIsEmpty_returnsFilmsWithStatusUnchanged() async {
-        let testPersistenceController = try! PersistenceController(inMemory: true)
-        let sut = FilmSyncService(context: testPersistenceController.viewContext)
+    func filmSyncService_syncFilmsWithLocalStorage_ifDatabaseIsEmpty_returnsFilmsWithStatusUnchanged() async throws {
+        let (sut, _, _) = try makeSUTViewContextAndEntity()
         let films = [Film.sample[0]]
         
         let result = await sut.syncFilmsWithLocalStorage(films)
@@ -38,13 +36,7 @@ struct FilmSyncServiceTests {
     
     @Test("If database is not empty but records don't match the input, should return the input films with their default flags unchanged.")
     func filmSyncService_syncFilmsWithLocalStorage_ifDatabaseRecordsDontMatchInput_returnsInputWithStatusUnchanged() async throws {
-        let testPersistenceController = try! PersistenceController(inMemory: true)
-        let context = testPersistenceController.viewContext
-        let sut = FilmSyncService(context: context)
-        let entity = try #require(
-            NSEntityDescription.entity(forEntityName: "FilmMO", in: context),
-            "The Core Data model schema must contain an entity definition named 'FilmMO'."
-        )
+        let (sut, context, entity) = try makeSUTViewContextAndEntity()
         _ = PersistenceHelper.makeFilmMO(with: Film.sample[0], entity: entity, context: context, isUpNext: true, isWatched: true)
         try? context.save()
         let filmB = [Film.sample[1]]
@@ -54,5 +46,19 @@ struct FilmSyncServiceTests {
         #expect(result == filmB, "Output should match input.")
         #expect(result[0].isUpNext == false, "Should be false (unchanged).")
         #expect(result[0].isWatched == false, "Should be false (unchanged).")
+    }
+    
+    // MARK: - SUT Helper Method
+    private func makeSUTViewContextAndEntity() throws -> (sut: FilmSyncService,
+                                                          viewContext: NSManagedObjectContext,
+                                                          entity: NSEntityDescription) {
+        let testPersistenceController = try! PersistenceController(inMemory: true)
+        let context = testPersistenceController.viewContext
+        let entity = try #require(
+            NSEntityDescription.entity(forEntityName: "FilmMO", in: context),
+            "The Core Data model schema must contain an entity definition named 'FilmMO'."
+        )
+        let sut = FilmSyncService(context: context)
+        return (sut, context, entity)
     }
 }
