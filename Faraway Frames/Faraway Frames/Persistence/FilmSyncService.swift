6df80366c8d9b8e6137055962bd0b96b1fa05902
Steven Hill <98730693+steven-hill@users.noreply.github.com>
2026-06-20 12:41:07 +0100

    add property on `FilmSyncService` for a managed object context

--- Faraway Frames/Faraway Frames/Persistence/FilmSyncService.swift
+++ Faraway Frames/Faraway Frames/Persistence/FilmSyncService.swift
@@ -6,8 +6,15 @@
 //
 
 import Foundation
+import CoreData
 
 final class FilmSyncService {
+    private let context: NSManagedObjectContext
+    
+    init(context: NSManagedObjectContext) {
+        self.context = context
+    }
+    
     func syncFilmsWithLocalStorage(_ films: [Film]) async -> [Film] {
         guard !films.isEmpty else { return [] }
         return films
--- Faraway Frames/Faraway FramesUnitTests/PersistenceTests/FilmSyncServiceTests.swift
+++ Faraway Frames/Faraway FramesUnitTests/PersistenceTests/FilmSyncServiceTests.swift
@@ -8,15 +8,30 @@
 import Testing
 @testable import Faraway_Frames
 
+@MainActor
 struct FilmSyncServiceTests {
 
     @Test("Should exit immediately if input films array is empty")
     func filmSyncService_syncFilmsWithLocalStorage_ifFilmsArrayIsEmpty_exitsImmediately() async {
-        let sut = FilmSyncService()
+        let testPersistenceController = try! PersistenceController(inMemory: true)
+        let sut = FilmSyncService(context: testPersistenceController.viewContext)
         let films: [Film] = []
         
         let result = await sut.syncFilmsWithLocalStorage(films)
         
         #expect(result.isEmpty, "Should be empty.")
+    }
+
+    @Test("If database contains no matching records, should return the original remote films with their default flags unchanged.")
+    func filmSyncService_syncFilmsWithLocalStorage_ifDatabaseRecordsDontMatchFilms_returnsFilmsWithStatusUnchanged() async {
+        let testPersistenceController = try! PersistenceController(inMemory: true)
+        let sut = FilmSyncService(context: testPersistenceController.viewContext)
+        let films = [Film.sample[0]]
+        
+        let result = await sut.syncFilmsWithLocalStorage(films)
+        
+        #expect(result == films, "Should match.")
+        #expect(result[0].isUpNext == false, "Should be false (unchanged).")
+        #expect(result[0].isWatched == false, "Should be false (unchanged).")
     }
 }
