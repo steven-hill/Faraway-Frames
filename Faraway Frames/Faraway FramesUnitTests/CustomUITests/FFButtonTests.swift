e7bb018d681c38cd9fad4e2a1a78c4558332b9c6
Steven Hill <98730693+steven-hill@users.noreply.github.com>
2026-06-24 18:33:19 +0100

    add new test file in new folder

---
+++ Faraway Frames/Faraway FramesUnitTests/CustomUITests/FFButtonTests.swift
@@ -0,0 +1,14 @@
+//
+//  FFButtonTests.swift
+//  Faraway FramesTests
+//
+//  Created by Steven Hill on 24/06/2026.
+//
+
+import Testing
+
+struct FFButtonTests {
+
+    @Test func doNothing() {
+    }
+}
