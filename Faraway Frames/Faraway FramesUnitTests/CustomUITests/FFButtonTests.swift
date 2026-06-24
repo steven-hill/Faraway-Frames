dff59356ab3f749d53faa01fb3794f298b6f4c70
Steven Hill <98730693+steven-hill@users.noreply.github.com>
2026-06-24 18:44:10 +0100

    add unit test to check certain FFButton's can be modified

--- Faraway Frames/Faraway FramesUnitTests/CustomUITests/FFButtonTests.swift
+++ Faraway Frames/Faraway FramesUnitTests/CustomUITests/FFButtonTests.swift
@@ -6,9 +6,30 @@
 //
 
 import Testing
+@testable import Faraway_Frames
+import UIKit
 
 struct FFButtonTests {
 
-    @Test func doNothing() {
+    @Test("Button supports changing its title, image and accessibility properties.")
+    func ffButton_update_modifiesTitleImageAndAccessibilityProperties() {
+        let sut = FFButton(
+            title: "Initial Title",
+            systemImageName: "plus",
+            accessibilityIdentifier: "test_id",
+            accessibilityHint: "Initial Hint"
+        )
+        
+        sut.update(
+            title: "New Title",
+            systemImageName: "minus",
+            accessibilityIdentifier: "new_id",
+            accessibilityHint: "New Hint"
+        )
+        
+        #expect(sut.configuration?.title == "New Title", "Should match.")
+        #expect(sut.configuration?.image == UIImage(systemName: "minus"), "Should match.")
+        #expect(sut.accessibilityIdentifier == "new_id", "Should match.")
+        #expect(sut.accessibilityHint == "New Hint", "Should match.")
     }
 }
