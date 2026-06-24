620d2d038808a9224d73463f96bee88d3785a169
Steven Hill <98730693+steven-hill@users.noreply.github.com>
2026-06-24 19:07:14 +0100

    fix the test

--- Faraway Frames/Faraway FramesUnitTests/CustomUITests/FFButtonTests.swift
+++ Faraway Frames/Faraway FramesUnitTests/CustomUITests/FFButtonTests.swift
@@ -9,27 +9,26 @@ import Testing
 @testable import Faraway_Frames
 import UIKit
 
+@MainActor
 struct FFButtonTests {
 
-    @Test("Button supports changing its title, image and accessibility properties.")
-    func ffButton_update_modifiesTitleImageAndAccessibilityProperties() {
+    @Test("Button supports changing its title, image and accessibility properties while others remain unchanged.")
+    func ffButton_update_modifiesTitleImageAndAccessibilityPropertiesOnly() {
+        let testVC = UIViewController()
         let sut = FFButton(
-            title: "Initial Title",
-            systemImageName: "plus",
-            accessibilityIdentifier: "test_id",
-            accessibilityHint: "Initial Hint"
-        )
+                    title: "Initial Title",
+                    systemImageName: "plus",
+                    accessibilityIdentifier: "test_id",
+                    accessibilityHint: "Initial Hint"
+                )
+        testVC.view.addSubview(sut)
+        testVC.loadViewIfNeeded()
         
-        sut.update(
-            title: "New Title",
-            systemImageName: "minus",
-            accessibilityIdentifier: "new_id",
-            accessibilityHint: "New Hint"
-        )
+        sut.update(title: "New Title", systemImageName: "minus", accessibilityHint: "New Hint")
         
-        #expect(sut.configuration?.title == "New Title", "Should match.")
-        #expect(sut.configuration?.image == UIImage(systemName: "minus"), "Should match.")
-        #expect(sut.accessibilityIdentifier == "new_id", "Should match.")
-        #expect(sut.accessibilityHint == "New Hint", "Should match.")
+        #expect(sut.configuration?.title == "New Title", "Should have been updated.")
+        #expect(sut.configuration?.image == UIImage(systemName: "minus"), "Should have been updated.")
+        #expect(sut.accessibilityHint == "New Hint", "Should have been updated.")
+        #expect(sut.accessibilityIdentifier == "test_id", "Should be unchanged.")
     }
 }
