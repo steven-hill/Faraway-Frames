1ed6c69e4301f5044025f8fcc215574682fcb530
Steven Hill <98730693+steven-hill@users.noreply.github.com>
2026-07-04 22:10:15 +0100

    moved UI testing code into an extension

--- Faraway Frames/Faraway Frames/ViewModels/FilmDetail/FilmDetailViewModel.swift
+++ Faraway Frames/Faraway Frames/ViewModels/FilmDetail/FilmDetailViewModel.swift
@@ -137,12 +137,7 @@ class FilmDetailViewModel {
             attemptingToUpdateFilm = true
             
             #if DEBUG
-            let env = ProcessInfo.processInfo.environment
-            if ProcessInfo.processInfo.isUITestingPersistenceSaveError {
-                let failureReason = env["MOCK_CD_FAILURE_REASON"]
-                let mappedReason: CocoaError.Code = (failureReason == "diskFull") ? .fileWriteOutOfSpace : .persistentStoreOpen
-                throw CocoaError(mappedReason)
-            }
+            try throwErrorForUITests()
             #endif
          
             let didStatusChange = try await filmQueueService.updateFilmStatus(film: film, queue: queue, action: action)
@@ -176,5 +171,16 @@ class FilmDetailViewModel {
     
     func returnToFilmContent(film: Film) {
         setFilm(film)
+    }
+}
+
+private extension FilmDetailViewModel {
+    private func throwErrorForUITests() throws {
+        let env = ProcessInfo.processInfo.environment
+        if ProcessInfo.processInfo.isUITestingPersistenceSaveError {
+            let failureReason = env["MOCK_CD_FAILURE_REASON"]
+            let mappedReason: CocoaError.Code = (failureReason == "diskFull") ? .fileWriteOutOfSpace : .persistentStoreOpen
+            throw CocoaError(mappedReason)
+        }
     }
 }
