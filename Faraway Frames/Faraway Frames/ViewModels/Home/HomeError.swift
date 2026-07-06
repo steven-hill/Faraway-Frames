9bf58d678d2fc77e643e74c0b43aef7f6e2ef158
Steven Hill <98730693+steven-hill@users.noreply.github.com>
2026-07-06 21:18:05 +0100

    update `HomeError` to use `PersistenceFailureReason` and delete `mapReason`

--- Faraway Frames/Faraway Frames/ViewModels/Home/HomeError.swift
+++ Faraway Frames/Faraway Frames/ViewModels/Home/HomeError.swift
@@ -9,27 +9,9 @@ import Foundation
 import CoreData
 
 enum HomeError: Error, Equatable {
-    
-    enum FailureReason: Equatable {
-        case diskFull
-        case databaseError
-        case unknown(String)
-        
-        var message: String {
-            switch self {
-            case .diskFull:
-                return "Your device storage is full. Free up space and try again."
-            case .databaseError:
-                return "There was a problem with the database. Please try again."
-            case .unknown(let message):
-                return message
-            }
-        }
-    }
-    
-    case fetchFailed(FailureReason)
-    case addFailed(FailureReason)
-    case deleteFailed(FailureReason)
+    case fetchFailed(PersistenceFailureReason)
+    case addFailed(PersistenceFailureReason)
+    case deleteFailed(PersistenceFailureReason)
 }
 
 // MARK: - User-facing Descriptions
@@ -47,6 +29,7 @@ extension HomeError: LocalizedError {
 }
 
 extension HomeError {
+    @MainActor
     var secondaryText: String {
         switch self {
         case .fetchFailed(let reason),
@@ -55,25 +38,4 @@ extension HomeError {
             return reason.message
         }
     }
-}
-
-// MARK: - Factory Mappings
-extension HomeError {
-    private static func mapReason(_ error: Error) -> FailureReason {
-        guard let cocoaError = error as? CocoaError else {
-            return .unknown(error.localizedDescription)
-        }
-        switch cocoaError.code {
-        case .fileWriteOutOfSpace:
-            return .diskFull
-        case .persistentStoreOpen, .persistentStoreTypeMismatch, .managedObjectReferentialIntegrity:
-            return .databaseError
-        default:
-            return .databaseError
-        }
-    }
-    
-    static func fetch(_ error: Error) -> Self { .fetchFailed(mapReason(error)) }
-    static func add(_ error: Error) -> Self { .addFailed(mapReason(error)) }
-    static func delete(_ error: Error) -> Self { .deleteFailed(mapReason(error)) }
 }
