889e79c4569c55a94ba671674d0d7059b2d0f107
Steven Hill <98730693+steven-hill@users.noreply.github.com>
2026-07-21 15:23:53 +0100

    refactoring for sut helper method and test renaming

--- Faraway Frames/Faraway FramesUnitTests/CoordinatorTests/HomeCoordinatorTests.swift
+++ Faraway Frames/Faraway FramesUnitTests/CoordinatorTests/HomeCoordinatorTests.swift
@@ -13,6 +13,29 @@ import UIKit
 struct HomeCoordinatorTests {
     
     @Test func homeCoordinator_start_placesViewControllerOnNavigationStack() {
+        let sut = makeSUT()
+
+        sut.start()
+        
+        #expect(sut.navigationController.viewControllers.count == 1, "Should push 1 view controller onto the navigation stack.")
+        #expect(sut.navigationController.viewControllers.first is HomeVC, "Should be `HomeVC`.")
+    }
+
+    @Test("HomeCoordinator forwards film from home view model up to delegate")
+    func homeCoordinator_homeViewModelDidCaptureFilm_bubblesEventUpToDelegate() {
+        let sut = makeSUT()
+        let delegateSpy = HomeCoordinatorDelegateSpy()
+        sut.delegate = delegateSpy
+        let film = Film.sample[0]
+        
+        sut.homeViewModelDidCaptureFilm(film)
+        
+        #expect(delegateSpy.didRequestNavigationCallCount == 1, "Should have called delegate method once.")
+        #expect(delegateSpy.capturedFilm?.id == film.id, "Should match the film passed to delegate method.")
+    }
+    
+    // MARK: - SUT Helper Method
+    private func makeSUT() -> HomeCoordinator {
         let testPersistenceController = try! PersistenceController(inMemory: true)
         let filmQueueService = FilmQueueService(context: testPersistenceController.viewContext)
         let sut = HomeCoordinator(navigationController: UINavigationController(),
@@ -20,13 +43,10 @@ struct HomeCoordinatorTests {
                                   imageLoader: MockImageLoader(),
                                   filmQueueService: filmQueueService
         )
-
-        sut.start()
-        
-        #expect(sut.navigationController.viewControllers.count == 1, "Should push 1 view controller onto the navigation stack.")
-        #expect(sut.navigationController.viewControllers.first is HomeVC, "Should be `HomeVC`.")
+        return sut
     }
     
+    // MARK: - Home Coordinator Delegate Spy
     final class HomeCoordinatorDelegateSpy: HomeCoordinatorDelegate {
         var didRequestNavigationCallCount = 0
         var capturedFilm: Film?
@@ -35,24 +55,5 @@ struct HomeCoordinatorTests {
             didRequestNavigationCallCount = 1
             capturedFilm = film
         }
-    }
-
-    @Test("HomeCoordinator forwards the selection event's film up to delegate")
-    func homeCoordinator_homeViewModelDidSelectFilm_bubblesEventUpToDelegate() {
-        let testPersistenceController = try! PersistenceController(inMemory: true)
-        let filmQueueService = FilmQueueService(context: testPersistenceController.viewContext)
-        let sut = HomeCoordinator(navigationController: UINavigationController(),
-                                  context: testPersistenceController.viewContext,
-                                  imageLoader: MockImageLoader(),
-                                  filmQueueService: filmQueueService
-        )
-        let delegateSpy = HomeCoordinatorDelegateSpy()
-        sut.delegate = delegateSpy
-        let film = Film.sample[0]
-        
-        sut.homeViewModelDidCaptureFilm(film)
-        
-        #expect(delegateSpy.didRequestNavigationCallCount == 0, "Should have called delegate method once.")
-        #expect(delegateSpy.capturedFilm?.title == "Batman", "Should match the film passed to delegate method.")
     }
 }
