fe3f56b4f05924b9433683e0c9f1652a1febf5a8
Steven Hill <98730693+steven-hill@users.noreply.github.com>
2026-07-21 14:58:32 +0100

    refactor test code using specific sut helper method for these two unit tests

--- Faraway Frames/Faraway FramesUnitTests/ViewControllerTests/HomeVCTests.swift
+++ Faraway Frames/Faraway FramesUnitTests/ViewControllerTests/HomeVCTests.swift
@@ -251,37 +251,11 @@ struct HomeVCTests {
         
         #expect(cell.currentDisplayedImage == nil, "Should be nil if image is not in cache.")
     }
-    
-    final class SpyCoordinatorDelegate: HomeViewModelCoordinatorDelegate {
-        var didCaptureFilmCallCount = 0
-        var capturedFilm: Film?
-        
-        func homeViewModelDidCaptureFilm(_ film: Film) {
-            didCaptureFilmCallCount = 1
-            capturedFilm = film
-        }
-    }
 
     @Test("Tapping a cell in `Up Next` flows through the view model and fires the coordinator delegate")
     func homeVC_didSelectItemAt_filmInUpNext_flowsThroughViewModelToCoordinatorDelegate() async throws {
         let upNextFilm = Film.sample[0]
-        let testPersistenceController = try! PersistenceController(inMemory: true)
-        let context = testPersistenceController.viewContext
-        let mockUpNextFRC = PersistenceHelper.makeMockUpNextFRC(context: context)
-        let mockWatchedFRC = PersistenceHelper.makeMockWatchedFRC(context: context)
-        let filmQueueService = FilmQueueService(context: context)
-        let homeVM = HomeViewModel(
-            upNextFRC: mockUpNextFRC,
-            watchedFRC: mockWatchedFRC,
-            imageLoader: MockImageLoader(),
-            filmQueueService: filmQueueService
-        )
-        let spyDelegate = SpyCoordinatorDelegate()
-        homeVM.coordinatorDelegate = spyDelegate
-        let sut = HomeVC(homeViewModel: homeVM)
-        let entity = try #require(
-            NSEntityDescription.entity(forEntityName: "FilmMO", in: context),
-            "The Core Data model schema must contain an entity definition named 'FilmMO'.")
+        let (sut, context, spyDelegate, entity) = try makeSUTForCellTap()
         _ = PersistenceHelper.makeFilmMO(with: upNextFilm,
                                          entity: entity,
                                          context: context,
@@ -304,23 +278,7 @@ struct HomeVCTests {
     func homeVC_didSelectItemAt_filmInWatched_flowsThroughViewModelToCoordinatorDelegate() async throws {
         let upNextFilm = Film.sample[0]
         let watchedFilm = Film.sample[1]
-        let testPersistenceController = try! PersistenceController(inMemory: true)
-        let context = testPersistenceController.viewContext
-        let mockUpNextFRC = PersistenceHelper.makeMockUpNextFRC(context: context)
-        let mockWatchedFRC = PersistenceHelper.makeMockWatchedFRC(context: context)
-        let filmQueueService = FilmQueueService(context: context)
-        let homeVM = HomeViewModel(
-            upNextFRC: mockUpNextFRC,
-            watchedFRC: mockWatchedFRC,
-            imageLoader: MockImageLoader(),
-            filmQueueService: filmQueueService
-        )
-        let spyDelegate = SpyCoordinatorDelegate()
-        homeVM.coordinatorDelegate = spyDelegate
-        let sut = HomeVC(homeViewModel: homeVM)
-        let entity = try #require(
-            NSEntityDescription.entity(forEntityName: "FilmMO", in: context),
-            "The Core Data model schema must contain an entity definition named 'FilmMO'.")
+        let (sut, context, spyDelegate, entity) = try makeSUTForCellTap()
         _ = PersistenceHelper.makeFilmMO(with: upNextFilm,
                                          entity: entity,
                                          context: context,
@@ -406,11 +364,46 @@ struct HomeVCTests {
         return (sut, context, entity)
     }
     
+    private func makeSUTForCellTap() throws -> (sut: HomeVC,
+                                                context: NSManagedObjectContext,
+                                                spyDelegate: SpyCoordinatorDelegate,
+                                                entity: NSEntityDescription) {
+        let testPersistenceController = try! PersistenceController(inMemory: true)
+        let context = testPersistenceController.viewContext
+        let mockUpNextFRC = PersistenceHelper.makeMockUpNextFRC(context: context)
+        let mockWatchedFRC = PersistenceHelper.makeMockWatchedFRC(context: context)
+        let filmQueueService = FilmQueueService(context: context)
+        let homeVM = HomeViewModel(
+            upNextFRC: mockUpNextFRC,
+            watchedFRC: mockWatchedFRC,
+            imageLoader: MockImageLoader(),
+            filmQueueService: filmQueueService
+        )
+        let spyDelegate = SpyCoordinatorDelegate()
+        homeVM.coordinatorDelegate = spyDelegate
+        let sut = HomeVC(homeViewModel: homeVM)
+        let entity = try #require(
+            NSEntityDescription.entity(forEntityName: "FilmMO", in: context),
+            "The Core Data model schema must contain an entity definition named 'FilmMO'.")
+        return (sut, context, spyDelegate, entity)
+    }
+    
     //MARK: - Collection View Header Helper
     private func getHeader(sut: HomeVC) -> SegmentedControlHeaderView? {
         let indexPath = IndexPath(item: 0, section: 0)
         let kind = UICollectionView.elementKindSectionHeader
         let header = sut.collectionView.supplementaryView(forElementKind: kind, at: indexPath) as? SegmentedControlHeaderView
         return header
+    }
+    
+    //MARK: - Home View Model Coordinator Delegate Spy
+    final class SpyCoordinatorDelegate: HomeViewModelCoordinatorDelegate {
+        var didCaptureFilmCallCount = 0
+        var capturedFilm: Film?
+        
+        func homeViewModelDidCaptureFilm(_ film: Film) {
+            didCaptureFilmCallCount = 1
+            capturedFilm = film
+        }
     }
 }
