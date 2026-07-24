d728e4db4211fe15a9e2d693b9ad4ffd85a9d5f5
Steven Hill <98730693+steven-hill@users.noreply.github.com>
2026-07-24 20:04:53 +0100

    add test file in folder

---
+++ Faraway Frames/Faraway FramesUnitTests/ConfiguratorTests/FilmRowCellConfiguratorTests.swift
@@ -0,0 +1,14 @@
+//
+//  FilmRowCellConfiguratorTests.swift
+//  Faraway FramesTests
+//
+//  Created by Steven Hill on 24/07/2026.
+//
+
+import Testing
+
+struct FilmRowCellConfiguratorTests {
+
+    @Test func doNothing() async throws {
+    }
+}
