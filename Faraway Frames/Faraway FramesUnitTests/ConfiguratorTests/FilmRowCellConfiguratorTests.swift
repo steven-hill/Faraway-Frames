4a078deeed479331a6939079d4361c7c3b846db0
Steven Hill <98730693+steven-hill@users.noreply.github.com>
2026-07-24 20:18:57 +0100

    add test for configuring the cell's title

--- Faraway Frames/Faraway FramesUnitTests/ConfiguratorTests/FilmRowCellConfiguratorTests.swift
+++ Faraway Frames/Faraway FramesUnitTests/ConfiguratorTests/FilmRowCellConfiguratorTests.swift
@@ -6,9 +6,47 @@
 //
 
 import Testing
+@testable import Faraway_Frames
+import UIKit
 
+@MainActor
 struct FilmRowCellConfiguratorTests {
-
-    @Test func doNothing() async throws {
+    
+    @Test("Configuring a cell sets the title text")
+    func filmRowCellConfigurator_configure_setsTitle() {
+        let film = Film.sample[0]
+        let spy = FilmRowCellSpy()
+        let mockService = MockFilmsListService()
+        let mockImageLoader = MockImageLoader()
+        let testPersistenceController = try! PersistenceController(inMemory: true)
+        let filmSyncService = FilmSyncService(context: testPersistenceController.viewContext)
+        let filmsListViewModel = FilmsListViewModel(filmsListService: mockService,
+                                                    imageLoader: mockImageLoader,
+                                                    filmSyncService: filmSyncService)
+        let sut = FilmRowCellConfigurator(viewModel: filmsListViewModel)
+        
+        sut.configure(spy, with: film)
+        
+        #expect(spy.configureTitleCalledCallCount == 1, "Should have called configureTitle once.")
+        #expect(spy.lastConfiguredTitle == film.title, "Should match the film title.")
+    }
+    
+    // MARK: - Spy Cell
+    final class FilmRowCellSpy: FilmRowCellRepresentable {
+        var imageTask: Task<Void, Never>?
+        var configureTitleCalledCallCount = 0
+        var configureImageCalledCallCount = 0
+        var lastConfiguredTitle: String?
+        var lastConfiguredImage: UIImage?
+        
+        func configureTitle(title: String) {
+            configureTitleCalledCallCount += 1
+            lastConfiguredTitle = title
+        }
+        
+        func configureImage(image: UIImage?) {
+            configureImageCalledCallCount += 1
+            lastConfiguredImage = image
+        }
     }
 }
