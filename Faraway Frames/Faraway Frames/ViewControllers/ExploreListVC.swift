7a183a13f91ca93cc759a76cb0c036bc4865a5d6
Steven Hill <98730693+steven-hill@users.noreply.github.com>
2026-07-25 14:32:14 +0100

    delete redundant line of code

--- Faraway Frames/Faraway Frames/ViewControllers/ExploreListVC.swift
+++ Faraway Frames/Faraway Frames/ViewControllers/ExploreListVC.swift
@@ -89,7 +89,6 @@ final class ExploreListVC: UIViewController {
             guard let self = self else { return nil }
             var config = UICollectionLayoutListConfiguration(appearance: .sidebar)
             config.backgroundColor = .systemBackground
-            config.showsSeparators = true
             config.headerMode = self.viewModel.currentState == .content(isUsingArchivedData: true) ? .supplementary : .none
             return NSCollectionLayoutSection.list(using: config, layoutEnvironment: layoutEnvironment)
         }
