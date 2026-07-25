0e41e0f093aff044f67c1c1b68cfee42e47ece5f
Steven Hill <98730693+steven-hill@users.noreply.github.com>
2026-07-25 14:31:41 +0100

    change appearance to `.sidebar`

--- Faraway Frames/Faraway Frames/ViewControllers/ExploreListVC.swift
+++ Faraway Frames/Faraway Frames/ViewControllers/ExploreListVC.swift
@@ -87,7 +87,7 @@ final class ExploreListVC: UIViewController {
     private func createLayout() -> UICollectionViewLayout {
         let sectionProvider = { [weak self] (sectionIndex: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
             guard let self = self else { return nil }
-            var config = UICollectionLayoutListConfiguration(appearance: CurrentDevice.isIPhone ? .plain : .sidebar)
+            var config = UICollectionLayoutListConfiguration(appearance: .sidebar)
             config.backgroundColor = .systemBackground
             config.showsSeparators = true
             config.headerMode = self.viewModel.currentState == .content(isUsingArchivedData: true) ? .supplementary : .none
