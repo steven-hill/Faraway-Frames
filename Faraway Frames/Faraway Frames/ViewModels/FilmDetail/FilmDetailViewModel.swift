920d7907da5cb885e0822fa44aa88f9d38cf9724
Steven Hill <98730693+steven-hill@users.noreply.github.com>
2026-08-05 20:43:23 +0100

    refactor `FilmDetailViewModel.throwErrorForUITests()`

--- Faraway Frames/Faraway Frames/ViewModels/FilmDetail/FilmDetailViewModel.swift
+++ Faraway Frames/Faraway Frames/ViewModels/FilmDetail/FilmDetailViewModel.swift
@@ -265,16 +265,20 @@ extension FilmDetailViewModel: NSFetchedResultsControllerDelegate {
 // MARK: - Extension for setting up UI tests
 private extension FilmDetailViewModel {
     private func throwErrorForUITests() throws {
-        let env = ProcessInfo.processInfo.environment
-        if ProcessInfo.processInfo.isUITestingPersistenceSaveError {
-            let failureReason = env["MOCK_CD_FAILURE_REASON"]
-            let mappedReason: CocoaError.Code = (failureReason == "diskFull") ? .fileWriteOutOfSpace : .persistentStoreOpen
-            throw CocoaError(mappedReason)
+        let processInfo = ProcessInfo.processInfo
+        let isSaveError = processInfo.isUITestingPersistenceSaveError
+        let isLoadError = processInfo.isUITestingExploreDetailVCPersistenceLoadError
+        guard isSaveError || isLoadError else { return }
+        
+        let failureReason = processInfo.environment["MOCK_CD_FAILURE_REASON"]
+        let errorCode: CocoaError.Code
+        
+        if isSaveError {
+            errorCode = (failureReason == "diskFull") ? .fileWriteOutOfSpace : .persistentStoreOpen
+        } else { // Must be isLoadError due to the guard check above
+            errorCode = (failureReason == "databaseError") ? .coreData : .persistentStoreOpen
         }
-        if ProcessInfo.processInfo.isUITestingExploreDetailVCPersistenceLoadError {
-            let failureReason = env["MOCK_CD_FAILURE_REASON"]
-            let mappedReason: CocoaError.Code = (failureReason == "databaseError") ? .coreData : .persistentStoreOpen
-            throw CocoaError(mappedReason)
-        }
+        
+        throw CocoaError(errorCode)
     }
 }
