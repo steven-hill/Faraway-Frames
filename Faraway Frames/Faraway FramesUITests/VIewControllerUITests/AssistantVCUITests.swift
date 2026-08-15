ffe65250eb58d2e429302dbda59a6517c2d4992b
Steven Hill <98730693+steven-hill@users.noreply.github.com>
2026-08-15 14:23:15 +0100

    add a check for a close button to the UI test

--- Faraway Frames/Faraway FramesUITests/VIewControllerUITests/AssistantVCUITests.swift
+++ Faraway Frames/Faraway FramesUITests/VIewControllerUITests/AssistantVCUITests.swift
@@ -21,14 +21,16 @@ final class AssistantVCUITests: XCTestCase {
         app = nil
     }
 
-    func test_assistantVC_hasTitle() {
+    func test_assistantVC_hasTitleAndCloseButton() {
         NavigationHelper.navigateToExploreTab(app: app)
         app.collectionViews.element.cells.element(boundBy: 0).tap()
         app.swipeUp()
         let moreLikeThisButton = app.buttons["ExploreDetailVC_MoreLikeThisButton"]
         moreLikeThisButton.tap()
         let title = app.staticTexts["Assistant"]
+        let closeButton = app.buttons["AssistantVC_CloseButton"]
         
         XCTAssertTrue(title.exists, "Should have a title.")
+        XCTAssertTrue(closeButton.isHittable, "Should have a hittable close button.")
     }
 }
