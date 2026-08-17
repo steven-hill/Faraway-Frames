f2cc7e7c4ef273d10c4e045bb827e9ea6ae5414b
Steven Hill <98730693+steven-hill@users.noreply.github.com>
2026-08-17 18:53:36 +0100

    add view model and its test file

---
+++ Faraway Frames/Faraway Frames/ViewModels/Assistant/AssistantViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AssistantViewModel.swift
+//  Faraway Frames
+//
+//  Created by Steven Hill on 17/08/2026.
+//
+
+import Foundation
---
+++ Faraway Frames/Faraway FramesUnitTests/ViewModelTests/AssistantViewModelTests.swift
@@ -0,0 +1,8 @@
+//
+//  AssistantViewModelTests.swift
+//  Faraway FramesTests
+//
+//  Created by Steven Hill on 17/08/2026.
+//
+
+import Foundation
