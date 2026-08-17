3e6a8aaec4fa3275a3fa0923aed97e6c14343c87
Steven Hill <98730693+steven-hill@users.noreply.github.com>
2026-08-17 18:27:07 +0100

    add test file

---
+++ Faraway Frames/Faraway FramesUnitTests/FoundationModelClientTests.swift
@@ -0,0 +1,14 @@
+//
+//  FoundationModelClientTests.swift
+//  Faraway FramesTests
+//
+//  Created by Steven Hill on 17/08/2026.
+//
+
+import Testing
+
+struct FoundationModelClientTests {
+
+    @Test func doNothing() {
+    }
+}
