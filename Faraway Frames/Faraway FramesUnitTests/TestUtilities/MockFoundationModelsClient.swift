0a145be7cb600f448c95b61d783f6c410857e1aa
Steven Hill <98730693+steven-hill@users.noreply.github.com>
2026-08-18 20:41:34 +0100

    replace NSError with a stubbed error

--- Faraway Frames/Faraway FramesUnitTests/TestUtilities/MockFoundationModelsClient.swift
+++ Faraway Frames/Faraway FramesUnitTests/TestUtilities/MockFoundationModelsClient.swift
@@ -7,14 +7,13 @@
 
 import FoundationModels
 @testable import Faraway_Frames
-import Foundation
 
 final class MockFoundationModelsClient: FoundationModelsService {
     
     //MARK: - Stubs
     var stubbedAvailability: SystemLanguageModelStatus = .ready
-    var stubbedResponse = "Here are three films you might like..."
-    var shouldThrowError = false
+    let stubbedResponse = "Here are three films you might like..."
+    var stubbedError: TextGenerationError? = nil
     
     //MARK: - Spy property
     private(set) var generateResponseCallCount = 0
@@ -26,8 +25,8 @@ final class MockFoundationModelsClient: FoundationModelsService {
     
     func generateResponse(for film: String) async throws -> String {
         generateResponseCallCount += 1
-        if shouldThrowError {
-            throw NSError(domain: "MockError", code: -1, userInfo: nil)
+        if let error = stubbedError {
+            throw error
         } else {
             return stubbedResponse
         }
