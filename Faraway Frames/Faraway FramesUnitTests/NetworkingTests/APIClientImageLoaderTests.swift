c0bce59502951cab48dbf46f4d86beaa8e7491ba
Steven Hill <98730693+steven-hill@users.noreply.github.com>
2026-09-20 22:35:06 +0100

    code cleanup

--- Faraway Frames/Faraway FramesUnitTests/NetworkingTests/APIClientImageLoaderTests.swift
+++ Faraway Frames/Faraway FramesUnitTests/NetworkingTests/APIClientImageLoaderTests.swift
@@ -16,17 +16,22 @@ struct APIClientImageLoaderTests {
     func apiClientImageLoader_loadImage_whenSuccessful_shouldSaveImageToNSCache() async {
         let cacheManager = MockCacheManager()
         let testImage = makeTestImage()
-        let urlString = makeURLString()
         let mockResponse = HTTPURLResponse(
             url: URL(string: "https://ghibliapi.vercel.app")!,
             statusCode: 200,
             httpVersion: nil,
             headerFields: nil
         )!
-        let session = StubNetworkSession(data: testImage.pngData()!, response: mockResponse)
-        let sut = APIClientImageLoader(session: session, cacheManager: cacheManager)
+        let session = StubNetworkSession(
+            data: testImage.pngData()!,
+            response: mockResponse
+        )
+        let sut = APIClientImageLoader(
+            session: session,
+            cacheManager: cacheManager
+        )
         
-        let loadedImage = await sut.loadImage(for: urlString)
+        let loadedImage = await sut.loadImage(for: makeURLString())
         
         #expect(loadedImage != nil, "Should not be nil.")
         #expect(cacheManager.setDataCallCount == 1, "Should have been called once.")
@@ -35,24 +40,31 @@ struct APIClientImageLoaderTests {
     @Test(.tags(.networkRequest))
     func apiClientImageLoader_loadImage_ifThereIsAnError_shouldReturnNil() async {
         let cacheManager = MockCacheManager()
-        let urlString = makeURLString()
         let session = StubNetworkSession(error: APIError.noInternetConnection)
-        let sut = APIClientImageLoader(session: session, cacheManager: cacheManager)
+        let sut = APIClientImageLoader(
+            session: session,
+            cacheManager: cacheManager
+        )
         
-        let loadedImage = await sut.loadImage(for: urlString)
+        let loadedImage = await sut.loadImage(for: makeURLString())
         
         #expect(loadedImage == nil, "Should be nil.")
         #expect(cacheManager.setDataCallCount == 0, "Should not have been called.")
     }
     
     @Test func apiClientImageLoader_ifImageExistsInNSCache_shouldRetrieveImageFromNSCache() async {
-        let session = StubNetworkSession()
         let cacheManager = MockCacheManager()
         let urlString = makeURLString()
         let testImage = makeTestImage()
-        let sut = APIClientImageLoader(session: session, cacheManager: cacheManager)
+        let sut = APIClientImageLoader(
+            session: StubNetworkSession(),
+            cacheManager: cacheManager
+        )
         
-        cacheManager.setData(testImage, forKey: urlString as NSString)
+        cacheManager.setData(
+            testImage,
+            forKey: urlString as NSString
+        )
         let retrievedImage = await sut.loadImage(for: urlString)
         
         #expect(retrievedImage == testImage, "The retrieved image should be the one that was cached previously.")
@@ -63,7 +75,6 @@ struct APIClientImageLoaderTests {
     @Test func apiClientImageLoader_whenImageIsInURLCacheButNotNSCache_shouldRetrieveFromURLCacheAndAddToNSCache() async {
         let cacheManager = MockCacheManager()
         let testURL = makeTestURL()
-        let urlString = makeURLString()
         let testImage = makeTestImage()
         let imageData = testImage.pngData()!
         
@@ -73,16 +84,29 @@ struct APIClientImageLoaderTests {
             httpVersion: nil,
             headerFields: nil)!
                 
-        let testCache = URLCache(memoryCapacity: 10 * 1024, diskCapacity: 10 * 1024, diskPath: "test_URLCache")
-        let cachedResponse = CachedURLResponse(response: mockResponse, data: imageData)
+        let testCache = URLCache(
+            memoryCapacity: 10 * 1024,
+            diskCapacity: 10 * 1024,
+            diskPath: "test_URLCache"
+        )
+        let cachedResponse = CachedURLResponse(
+            response: mockResponse,
+            data: imageData
+        )
         testCache.removeAllCachedResponses()
-        testCache.storeCachedResponse(cachedResponse, for: URLRequest(url: testURL))
+        testCache.storeCachedResponse(
+            cachedResponse,
+            for: URLRequest(url: testURL)
+        )
         let configuration = URLSessionConfiguration.ephemeral
         configuration.urlCache = testCache
         let session = StubNetworkSession(configuration: configuration)
-        let sut = APIClientImageLoader(session: session, cacheManager: cacheManager)
+        let sut = APIClientImageLoader(
+            session: session,
+            cacheManager: cacheManager
+        )
         
-        let retrievedImage = await sut.loadImage(for: urlString)
+        let retrievedImage = await sut.loadImage(for: makeURLString())
         
         #expect(cacheManager.getDataCallCount == 1, "Should have checked NSCache first.")
         #expect(retrievedImage != nil, "Should have retrieved image from URLCache.")
@@ -90,12 +114,13 @@ struct APIClientImageLoaderTests {
     }
     
     @Test func apiClientImageLoader_checkCache_whenImageIsNotInEitherCache_shouldReturnNil() {
-        let session = StubNetworkSession()
         let cacheManager = MockCacheManager()
-        let urlString = makeURLString()
-        let sut = APIClientImageLoader(session: session, cacheManager: cacheManager)
+        let sut = APIClientImageLoader(
+            session: StubNetworkSession(),
+            cacheManager: cacheManager
+        )
         
-        let image = sut.checkCache(for: urlString)
+        let image = sut.checkCache(for: makeURLString())
         
         #expect(cacheManager.getDataCallCount == 1, "Should have been called once.")
         #expect(cacheManager.setDataCallCount == 0, "Should not have been called.")
